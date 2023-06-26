a60cd11815f61a1cca293d154560737a70267b93
jocelynszhu <90989505+jocelynszhu@users.noreply.github.com>
2023-06-26 16:29:11 -0400

    Update detect.py

--- gstreamer/detect.py
+++ gstreamer/detect.py
@@ -207,7 +207,7 @@ def main():
             return generate_svg(src_size, inference_size, inference_box, objs, labels, text_lines, trdata, trackerFlag)
 
     result = gstreamer.run_pipeline(user_callback,
-                                    src_size=(1280, 720),
+                                    src_size=(3000, 300),
                                     appsink_size=inference_size,
                                     trackerName=args.tracker,
                                     videosrc=args.videosrc,
